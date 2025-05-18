78b4d90592ff7b20fe8191666de5520a91907b81
NezraDev <nezraprogrammer@gmail.com>
2025-05-18 13:54:40 +0800

    feat : base.html

---
+++ static/script/image-maper.js
@@ -0,0 +1,10 @@
+$("img[usemap]").mapster({
+  fillColor: "643dff",
+  strokeColor: "643dff",
+  strokeOpacity: 0.8,
+  fillOpacity: 0.4,
+  strokeWidth: 2,
+  stroke: true,
+  singleSelect: true,
+  isSelectable: true,
+});
